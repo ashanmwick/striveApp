5ae78adb91b9cc7e86c2f68a6c308d7933ec39fb
Chathuka <chathukamalitha@gmail.com>
2023-02-04 22:45:49 +0530

    initial commit

---
+++ app/src/main/java/com/example/strive/ActivityFive.java
@@ -0,0 +1,14 @@
+package com.example.strive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityFive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_five);
+    }
+}
