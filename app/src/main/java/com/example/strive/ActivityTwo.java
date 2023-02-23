6fea57757bb23c7f693e04f1f0d38902de9791dc
Kavya Jayakody <kavyajs.21@uom.lk>
2023-02-23 07:36:30 +0530

    phone Authentication 2

--- app/src/main/java/com/example/strive/ActivityTwo.java
+++ app/src/main/java/com/example/strive/ActivityTwo.java
@@ -1,21 +1,18 @@
 package com.example.strive;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
@@ -24,7 +21,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ActivityTwo extends AppCompatActivity {
     EditText phone, otp;
-    Button btngenerateOTP;
+    Button btnGenerateOTP;
     FirebaseAuth mAuth;
     String verificationID;
 
@@ -35,25 +32,33 @@ public class ActivityTwo extends AppCompatActivity {
 
         phone = findViewById(R.id.editTextPhone);
         otp = findViewById(R.id.editTextNumberPassword);
-        btngenerateOTP = findViewById(R.id.button_otp);
+        btnGenerateOTP = findViewById(R.id.button_otp);
         mAuth = FirebaseAuth.getInstance();
 
-        btngenerateOTP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(TextUtils.isEmpty(phone.getText().toString())){
-                    Toast.makeText(ActivityTwo.this, "Enter Valid Phone No.", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    String number = phone.getText().toString();
-                    sendverificationcode(number);
-                }
+        btnGenerateOTP.setOnClickListener(view -> {
+            if(TextUtils.isEmpty(phone.getText().toString())){
+                Toast.makeText(ActivityTwo.this, "Enter Valid Phone No.", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                String number = phone.getText().toString();
+                sendVerificationCode(number);
             }
         });
 
     }
 
-    private void sendverificationcode(String phoneNumber) {
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+    }
+
+    private void sendVerificationCode(String phoneNumber) {
         PhoneAuthOptions options =
                 PhoneAuthOptions.newBuilder(mAuth)
                         .setPhoneNumber(phoneNumber)       // Phone number to verify
@@ -62,28 +67,35 @@ public class ActivityTwo extends AppCompatActivity {
                         .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
                         .build();
         PhoneAuthProvider.verifyPhoneNumber(options);
+
+
     }
 
-    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+
+    private final PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
         @Override
         public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
             final String code = credential.getSmsCode();
             if(code!=null){
-                verifycode(code);
+                verifyCode(code);
             }
         }
 
         @Override
         public void onVerificationFailed(@NonNull FirebaseException e) {
-
+            Toast.makeText(ActivityTwo.this, "Verification Failed!", Toast.LENGTH_SHORT).show();
         }
 
-        public void onVerificationFailed(@NonNull FirebaseException e, String s) {
+        /*@Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+
+        }*/
+
+        /*public void onVerificationFailed(@NonNull FirebaseException e, String s) {
             Toast.makeText(ActivityTwo.this, "Verification Failed!", Toast.LENGTH_SHORT).show();
-
-
-    };
+        }*/
 
         @Override
         public void onCodeSent(@NonNull String s,
@@ -91,22 +103,21 @@ public class ActivityTwo extends AppCompatActivity {
             super.onCodeSent(s, token);
             verificationID = s;
         }
-        private void verifycode(String code){
+
+        
+        private void verifyCode(String code){
             PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationID,code);
-            signinbyCredentials(credential);
+            signInByCredentials(credential);
         }
     };
 
-    private void signinbyCredentials(PhoneAuthCredential credential) {
+    private void signInByCredentials(PhoneAuthCredential credential) {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         firebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(ActivityTwo.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(ActivityTwo.this, ActivityThree.class));
-                        }
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        Toast.makeText(ActivityTwo.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(ActivityTwo.this, ActivityThree.class));
                     }
                 });
     }
