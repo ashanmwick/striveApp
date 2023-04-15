543b8910141c934531305c54ad4c2e1e6afed823
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2023-04-15 23:03:01 +0530

    Bluetooth Dummy sensor data background services added

--- app/src/main/java/com/example/strive/MainActivity.java
+++ app/src/main/java/com/example/strive/MainActivity.java
@@ -18,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Intent serviceIntent = new Intent(this,
+                bluetoothcomm.class);
+        startService(serviceIntent);
 
         h.postDelayed(new Runnable() {
             @Override
